60a587fcd5ad3624b59fa36d85dcf7e88ad96689
Martin Meltsas <martin.meltsas@khk.ee>
2015-10-07 01:38:34 +0300

    config.sample.php lisatud [Delivers #104984754]

---
+++ include/database.php
@@ -0,0 +1,5 @@
+<?php
+//connect DB
+require_once 'config.php';
+$db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
+mysqli_query($db, "SET NAMES 'utf8'");
--- post.php
+++ post.php
@@ -2,9 +2,8 @@
 // Get post id
 $post_id=isset($_GET['id']) ? $_GET['id'] : -1;
 
-//connect DB
-$db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
-mysqli_query($db, "SET NAMES 'utf8'");
+
+require 'include/database.php';
 
 //Retrieve data from DB
 $q = mysqli_query($db, "SELECT *, DATE_FORMAT(post_created, '%d.%m.%Y %H:%i') post_created FROM posts NATURAL JOIN authors WHERE post_id=$post_id");
