71336abf231fc3b3f83ed3d454ddefa1fe9da7fb
Martin Meltsas <martin.meltsas@khk.ee>
2015-10-07 02:53:22 +0300

    config.sample.php lisatud [Delivers #104984754]

--- config.sample.php
+++ config.sample.php
@@ -1,5 +1,5 @@
-<?php
+?php
 define('DATABASE_HOSTNAME', '127.0.0.1');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', 'cleanblog');
+define('DATABASE_DATABASE', PROJECT_NAME);
